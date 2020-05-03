f77db8fd911d28223aefddc4777aff2595a228da
Dale Seo <dale.seo@gmail.com>
2020-05-03 14:04:47 -0400

    write unit tests

---
+++ src/test.js
@@ -0,0 +1,32 @@
+import Remark from "remark";
+import toHAST from "mdast-util-to-hast";
+import hastToHTML from "hast-util-to-html";
+
+import { setParserPlugins } from "./";
+
+describe("gatsby-remark-line-breaks", () => {
+  test("A line break that is preceded by a backslash is parsed as a hard line break.", () => {
+    const remark = new Remark();
+    setParserPlugins().forEach((parserPlugins) => remark.use(parserPlugins));
+
+    const mdText = "foo\nbaz";
+    const mdAst = remark.parse(mdText);
+    const htmlAst = toHAST(mdAst, { allowDangerousHtml: true });
+    const htmlText = hastToHTML(htmlAst, { allowDangerousHtml: true });
+
+    expect(htmlText).toEqual("<p>foo<br>\nbaz</p>");
+  });
+
+  test("A line break that is preceded by two or more spaces is still parsed as a hard line break.", () => {
+    const remark = new Remark();
+    setParserPlugins().forEach((parserPlugins) => remark.use(parserPlugins));
+
+    const mdText = "foo  \nbaz";
+    const mdAst = remark.parse(mdText);
+    const htmlAst = toHAST(mdAst, { allowDangerousHtml: true });
+    const htmlText = hastToHTML(htmlAst, { allowDangerousHtml: true });
+
+    console.log(htmlText);
+    expect(htmlText).toEqual("<p>foo<br>\nbaz</p>");
+  });
+});
